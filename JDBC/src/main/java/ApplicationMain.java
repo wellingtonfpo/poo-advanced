4a1e7fe2db731b6929b58cb2940343b4d0b07dd9
Wellington Menezes <ligton.ribeiro@gmail.com>
2026-04-28 16:30:19 -0300

    simplify author retrieval and output in ApplicationMain.java

--- JDBC/src/main/java/ApplicationMain.java
+++ JDBC/src/main/java/ApplicationMain.java
@@ -1,13 +1,6 @@
-import models.Author;
 import repositories.AuthorRepository;
 
 void main() {
-    List<Author> authors;
     AuthorRepository authorRepository = new AuthorRepository();
-
-    authors = authorRepository.getAllAuthors();
-
-    for (Author author : authors) {
-        System.out.println(author);
-    }
+    authorRepository.getAllAuthors().forEach(System.out::println);
 }
